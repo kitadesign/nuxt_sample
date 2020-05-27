65542aee2e6d8e1abe9aa27b4a88439fcfc6f6d1
kitazawa <kitadesign@gmail.com>
2020-05-27 17:55:19 +0900

    first commit

---
+++ components/Star.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="block">
+    <h4>Nuxt.js + Element？</h4>
+    <el-rate v-model="value" :colors="['#2F3D4D', '#64B486', '#4B7C6E']"></el-rate>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Star',
+    data () {
+      return {
+        value: null
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .block {
+    padding: 30px 20px;
+  }
+</style>
